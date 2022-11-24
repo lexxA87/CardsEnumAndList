7a82e5d383b1460371434d635e1731c399887552
lexxA87 <alex.o.nikonov@gmail.com>
2022-11-24 15:30:03 +0400

    Added two enums Suits and Values

--- Values.cs
+++ Values.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CardsEnumAndList
+﻿namespace CardsEnumAndList
 {
-    internal class Values
+    enum Values
     {
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13,
+        Ace = 1,
     }
 }
